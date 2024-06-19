0cd81b10aec31321aef646a50cb93a10825571cb
plaraje <mat.demoya@gmail.com>
2024-06-19 13:38:00 +0200

    update

---
+++ PineUtils/main.py
@@ -0,0 +1,4 @@
+import classes.Array2D as Array2D
+import classes.Files as Files
+import classes.Input as Input
+import classes.Logger as Logger
---
+++ setup.py
@@ -0,0 +1,45 @@
+import setuptools 
+
+with open("README.md", "r") as fh: 
+	long_description = fh.read() 
+
+setuptools.setup( 
+	# Here is the module name. 
+	name="PineUtils", 
+
+	# version of the module 
+	version="0.0.1", 
+
+	# Name of Author 
+	author="Plaraje (DMAM2)", 
+
+	# your Email address 
+	author_email="plaraje@proton.me", 
+
+
+	# long_description=long_description, 
+
+	# Specifying that we are using markdown file for description 
+	long_description=long_description, 
+	long_description_content_type="text/markdown", 
+
+	# Any link to reach this module, ***if*** you have any webpage or github profile 
+	url="https://github.com/plaraje/PineUtils", 
+	packages=setuptools.find_packages(), 
+
+
+
+
+    install_requires=[ 
+        "Chromify", 
+    ], 
+
+
+	license="GNU AFFERO GENERAL PUBLIC LICENSE", 
+
+	# classifiers like program is suitable for python3, just leave as it is. 
+	classifiers=[ 
+		"Programming Language :: Python :: 3", 
+		"Operating System :: OS Independent", 
+	], 
+) 
